962e6e3f98b5140d4af22f067e8e7d423a21fb1d
Michael Droz <michael.droz@tcc-mdroz.local>
2014-11-11 20:39:45 -0800

    Initial bulk commit.

--- GeoCode3/ViewController.h
+++ GeoCode3/ViewController.h
@@ -7,9 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
+@property (strong, nonatomic) CLLocationManager *locationManager;
+
+@property (weak, nonatomic) IBOutlet CLLocation *location;
+
+- (IBAction)updateLocation:(id)sender;
+
+- (IBAction)monitorThisRegion:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *workLabel;
 
 @end
 
--- GeoCode3/ViewController.m
+++ GeoCode3/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+@import CoreLocation;
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
 
 @end
 
@@ -17,7 +19,85 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    self.locationManager.delegate = self;
+    
+    //Check for iOS 8. Wihtout this guard the code will crash with "unknonw select"
+    
+    if([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [self.locationManager requestWhenInUseAuthorization];
+    }
+    [self.locationManager startUpdatingLocation];
+    
 }
+
+- (IBAction)updateLocation:(UIBarButtonItem *)sender {
+    float spanX = 0.58;
+    float spanY = 0.58;
+    self.location = self.locationManager.location;
+    //a quick nslog to shus us that it's doing something
+    NSLog(@"About to print self.location.description");
+    NSLog(@"%@", self.location.description);
+    NSLog(@"This is a log to see where I am at");
+    NSLog(@"Test test test test");
+    MKCoordinateRegion region;
+    region.center.latitude = self.locationManager.location.coordinate.latitude;
+    region.center.longitude = self.locationManager.location.coordinate.longitude;
+    region.span = MKCoordinateSpanMake(spanX, spanY);
+    [self.mapView setRegion:region animated:YES];
+}
+
+- (IBAction)monitorThisRegion:(UIBarButtonItem *)sender  {
+
+   NSLog(@"I am in the montiroThisRegion method...");
+   float theRadius = 100.0;
+   NSString *theIdentifier = @"atWork";
+    NSLog(@"Set radius and the identifier");
+   CLLocationCoordinate2D centre;
+    centre.latitude = self.mapView.centerCoordinate.latitude;
+    centre.longitude = self.mapView.centerCoordinate.longitude;
+    NSLog(@"set the centre.latitude and centre.long");
+    CLCircularRegion *region = [[CLCircularRegion alloc] initWithCenter:centre
+                                                                 radius:theRadius
+                                                             identifier:theIdentifier
+                            ];
+    NSLog(@"Set the monitoring region with radius and identifier");
+    //Then cast the instance for use with your CLLocationManager instance
+   [self.locationManager startMonitoringForRegion:(CLRegion *)region];
+   NSLog(@"Just claled the startMonitoringFOrRegion method. Shit might start breaking");
+    
+    //Decprecated method.
+    /*[self.locationManager startMonitoringForRegion:[[CLRegion alloc] initCircularRegionWithCenter:centre radius:20.0 identifier:@"Work" ]]; */
+}
+
+//Location Manager Delegate Methods
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    NSLog(@"%@", [locations lastObject]);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+    [standardDefaults setObject:@"Yes" forKey:@"atWork"];
+    self.workLabel.text = @"You are at Work";
+    [standardDefaults synchronize];
+}
+
+
+-(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+    [standardDefaults setObject:@"No" forKey:@"atWork"];
+    self.workLabel.text = @"You are not at Work";
+    NSLog(@"YOU HAVE EXXXXXXXXXXXXXXXXXXXXXXITED");
+    [standardDefaults synchronize];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region {
+    NSLog(@"Now monitoring for FUCKKKKKKKKKKK%@", region.identifier);
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
